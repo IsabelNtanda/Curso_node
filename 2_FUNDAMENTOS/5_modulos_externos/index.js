18404b095aa9d6a374434ec38334a327fe35f18b
IsabelNtanda <isabelalvaro116@gmail.com>
2024-02-04 18:13:32 -0300

    minimist

---
+++ 2_FUNDAMENTOS/5_modulos_externos/index.js
@@ -0,0 +1,15 @@
+
+
+const minimist = require('minimist')
+
+const args = minimist(process.argv.slice(2))
+
+console.log(args)
+
+const nome = args['nome']
+
+const profissao = args['profissao']
+
+console.log(nome,profissao)
+
+console.log(`O nome dele é ${nome} e a profissao dele e ${profissao}`)
