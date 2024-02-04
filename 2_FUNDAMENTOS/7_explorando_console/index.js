21a0266832b7f57e924596f49de4c80775e98e66
IsabelNtanda <isabelalvaro116@gmail.com>
2024-02-04 18:44:06 -0300

    explorando o console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,21 @@
+
+
+const x = 10
+const y = 'Algun texto'
+const nome = 'Isabel'
+const z = [1,2]
+
+console.log(x,y,z)
+
+// console de impressões
+
+console.count(` O valor de x é : ${x}, contagem`)
+console.count(` O valor de x é : ${x}, contagem`)
+console.count(` O valor de x é : ${x}, contagem`)
+console.count(` O valor de x é : ${x}, contagem`)
+
+console.log('O nome é %s, ela é programadora',nome)
+
+setTimeout(()=>{
+console.clear()
+},1000)
