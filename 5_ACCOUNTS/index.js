1aa3226f85a63e6b5bf4e52a17803d43bafc71eb
IsabelNtanda <isabelalvaro116@gmail.com>
2024-02-27 20:24:30 -0300

    terminando criar conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -1,9 +1,41 @@
-
 // modulos externos
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 
 // modulos internos
 const fs = require('fs')
+operation()
 
-console.log('inicializamos ACCOUNT')
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'O que voce deseja fazer',
+        choices: [
+          'Criar Conta',
+          'Consultar Saldo',
+          'Depositar',
+          'Sacar',
+          'Sair',
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer['action']
+      if (action === 'Criar Conta') {
+        createAccount()
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+// Criar a conta
+
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
