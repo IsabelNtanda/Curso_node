8e308a0bc416595e6f50bfdef07593549d7c9172
IsabelNtanda <isabelalvaro116@gmail.com>
2024-02-27 21:00:32 -0300

    Finalizando criação de conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -38,4 +38,43 @@ function operation() {
 function createAccount() {
   console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
   console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+  buildingAccount()
+}
+// construindo criar a conta
+
+function buildingAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accoutname',
+        message: 'Digite um nome para a sua conta',
+      },
+    ])
+    .then((answer) => {
+      // const {accoutname} = answer
+      const accoutname = answer['accoutname']
+      console.info(accoutname)
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+
+      if (fs.existsSync(`accounts/${accoutname}.json`)) {
+        console.log(
+          chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+        )
+        buildingAccount()
+      }
+      fs.writeFileSync(
+        `accounts/${accoutname}.json`,
+        '{"balance":0}',
+        function (err) {
+          console.log(err)
+        },
+      )
+      console.log(chalk.green("Parabéns a sua conta foi criada"))
+      operation()
+    })
+    .catch((err) => console.log(err))
 }
